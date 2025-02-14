223a032dcc97d3df41b99225806c92bd01966ac7
RomuAnalyst <103258954+RomuAnalyst@users.noreply.github.com>
2025-02-14 23:22:55 +0100

    Update your_script.R

--- your_script.R
+++ your_script.R
@@ -1,10 +1,38 @@
-# your_script.R
-library(dplyr)
+library(tidyverse)
+library(httr2)
 
-# Créez un exemple de dataframe
-df <- data.frame(x = 1:10, y = rnorm(10))
+# Get forecast URL from NWS
+NWS_base_url <- 'https://api.weather.gov'
+NWS_response <- request(NWS_base_url) |> 
+  req_url_path_append(
+    'points',
+    '38.8894,-77.0352'
+  ) |> 
+  req_perform()
 
-# Écrivez le dataframe dans un fichier CSV
-write.csv(df, "results.csv", row.names = FALSE)
+# Extract forecast URL
+forecast_url <- NWS_response |> 
+  resp_body_json() |> 
+  pluck('properties', 'forecastHourly')
 
-print("Fichier CSV généré : output/results.csv")
+# Get actual forecast data
+forecast_response <- request(forecast_url) |> 
+  req_perform()
+
+# Bring data into tibble format
+extracted_data <- forecast_response |> 
+  resp_body_json() |> 
+  pluck('properties', 'periods') |> 
+  map_dfr( # iterates over each list and binds rows to a tibble
+    \(x) {
+      tibble(
+        time = x |> pluck('startTime'),
+        temp_F = x |> pluck('temperature'),
+        rain_prob = x |> pluck('probabilityOfPrecipitation', 'value'),
+        forecast = x |> pluck('shortForecast')
+      )
+    }
+  )
+extracted_data
+
+jsonlite::toJSON(extracted_data, pretty = TRUE)
